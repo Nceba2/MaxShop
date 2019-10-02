71da358812757303566165f8b625ab667674e1fd
Nceba2 <ncebasobikwa1@hotmail.com>
2019-10-02 23:20:43 +0200

    implement RestRequest to get info from api

---
+++ MaShop/Controllers/IRestController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace MaShop.Controllers
+{
+    interface IRestController
+    {
+        void ApiRequest_setResponse(string url);
+
+        JArray GetStyles();
+
+        string DoLogin(string[] Login_data);
+
+        string DoRegister(string[] Register_data);
+
+        string DoBooking(string[] Booking_data);
+    }
+
+}
---
+++ MaShop/Controllers/RestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MaShop.Controllers
+{
+    public class RestController : IRestController
+    {
+
+        JArray responseStr { get; set; }
+        public RestController()
+        {
+        }
+
+        public void ApiRequest_setResponse(string url)
+        {
+            using (var wb = new WebClient())
+            {
+                this.responseStr = JArray.Parse(wb.DownloadString(url));
+            }
+        }
+
+        public string DoBooking(string[] Booking_data)
+        {
+            return "booking";
+        }
+
+        public string DoLogin(string[] Login_data)
+        {
+            return "logging in";
+        }
+
+        public string DoRegister(string[] Register_data)
+        {
+            return "register";
+        }
+
+        public JArray GetStyles()
+        {
+            ApiRequest_setResponse("https://localhost:5002/api/values/table/styles");
+            return responseStr;
+        }
+    }
+}
