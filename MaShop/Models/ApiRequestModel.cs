dad7a220a7e6ce85e82f7bf4932b1869615a4c0a
Nceba2 <ncebasobikwa1@hotmail.com>
2019-10-05 17:44:53 +0200

    created universal request model to get api data

---
+++ MaShop/Models/ApiRequestModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace MaShop.Models
+{
+    public class ApiRequestModel: IApiRequestModel
+    {
+
+        JArray responseStr { get; set; }
+
+        public ApiRequestModel()
+        {
+        }
+
+        public JArray ApiGet(string url)
+        {
+            using (var wb = new WebClient())
+            {
+                this.responseStr = JArray.Parse(wb.DownloadString(url));
+            }
+            return responseStr;
+        }
+
+        public JArray ApiPost(string url, WebHeaderCollection wbString)
+        {
+            string resp;
+            using (var wb = new WebClient())
+            {
+                wb.Headers = wbString;
+
+                byte[] data = wb.UploadData(url, "POST", Encoding.Default.GetBytes("{\"test\": \"none\"}"));
+                resp = Encoding.ASCII.GetString(data);
+            }
+            this.responseStr = JArray.Parse(resp);
+
+            return responseStr;
+        }
+
+    }
+}
---
+++ MaShop/Models/IApiRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace MaShop.Models
+{
+    public interface IApiRequestModel
+    {
+        JArray ApiPost(string url, WebHeaderCollection wbString);
+        JArray ApiGet(string url);
+    }
+}
